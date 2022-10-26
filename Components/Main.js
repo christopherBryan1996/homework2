5ed33ff9bdf3768224b58a7d1ac71e249459d9c4
christopherBryanSMC <christopher@3ct.mx>
2022-10-25 20:08:26 -0500

    version 1.0.0

--- Components/Main.js
+++ Components/Main.js
@@ -1,13 +1,56 @@
-import {LitElement,html} from 'lit';
+import {LitElement,html,css} from 'lit';
 import { separateString } from '../action/stringAnalysis';
 
 class Main extends LitElement{
+    static get styles(){
+        return css`
+            .info{
+                display: flex;
+                flex-direction: row;
+                justify-content: center;
+                margin: 8px 40px 0px;
+            }
+            .divTable{
+                text-align: center;
+            }
+            p{
+                border: 1px solid;
+                padding: 2px 10px 6px;
+                margin: 0;
+                font-size: 2pc;
+            }
+        `
+    }
     constructor(){
         super();
         this.valueInput='';
         this.trueTable=[];
     }
+    _handleOnClickAnalis(e){
+        e.preventDefault()
+        let html=''
+        this.trueTable= separateString(this.valueInput)
+        this.trueTable.map((e)=>{
+            html+=`
+                <div class='divTable'>
+                    <p>${e[0]}</p>
+                    ${e[1].map((el)=>{
+                        console.log(el)
+                        return `
+                            <p>${el}</p>
+                        `
+                    })}
+                </div>
+            `
+        })
+        this.renderRoot.querySelector('.info').innerHTML=html.replace(/,/g, "")
+    }
+    _handleOnchangeValue(e){
+        this.valueInput=e.target.value
+    }
+    
     render(){
+        console.log(this.status)
         return html`
             <div>
                 <h2>Ejemplos:</h2>
@@ -17,21 +60,19 @@ class Main extends LitElement{
                     <li>~(p)</li>
                     <li>~(~p)</li>
                     <li>p ∧ q</li>
+                    <li>p ∨ q</li>
+                    <li>p ⨁ p</li>
+                    <li>p ⟹ q</li>
+                    <li>p ⟺ q</li>
                 </ul>
             </div>
             <form>
-                <input type="text" value="${this.valueInput}" @change="${this.handleOnchangeValue}"/>
-                <input type="submit" @click="${this.handleOnClickAnalis}"/>
+                <input type="text" value="${this.valueInput}" @change="${this._handleOnchangeValue}"/>
+                <input type="submit" @click="${this._handleOnClickAnalis}"/>
             </form>
+            <div class="info"></div>
+            
         `
-    }
-    handleOnClickAnalis(e){
-        e.preventDefault()
-        this.trueTable= separateString(this.valueInput)
-        console.log(this.trueTable)
-    }
-    handleOnchangeValue(e){
-        this.valueInput=e.target.value
     }
 };
 
--- action/stringAnalysis.js
+++ action/stringAnalysis.js
@@ -1,7 +1,12 @@
 export const separateString=(data)=>{
     const ddER= /^~[(]~[a-z][)]$/;
     const dER= /^~[(][a-z][)]$/;
-    const conjucion=/^[\s]*[a-z][\s]?∧[\s]?[a-z][\s]*$/
+    const conjucion=/^[\s]*[(]*[\s]*[a-z][\s]?∧[\s]?[a-z][\s]*[)]*[\s]*$/
+    const disyuncion=/^[\s]*[(]*[\s]*[a-z][\s]?∨[\s]?[a-z][\s]*[)]*[\s]*$/
+    const disyuncionE=/^[\s]*[(]*[\s]*[a-z][\s]?⨁ [\s]?[a-z][\s]*[)]*[\s]*$/
+    const condicionalOimplicacion=/^[\s]*[(]*[\s]*[a-z][\s]?⟹ [\s]?[a-z][\s]*[)]*[\s]*$/
+    const bicondicionalOequivalencia=/^[\s]*[(]*[\s]*[a-z][\s]?⟺[\s]?[a-z][\s]*[)]*[\s]*$/
+
     if(data.length==1)
         return [[data,['v','f']]]
     else if(data.length==2){
@@ -15,7 +20,15 @@ export const separateString=(data)=>{
     else if( dER.test(data))
         return oneDenial(data)
     else if(conjucion.test(data))
-        pConjucion(data)
+        return pConjucion(data)
+    else if(disyuncion.test(data))
+        return pdisyuncion(data)
+    else if(disyuncionE.test(data))
+        return pdisyuncionE(data)
+    else if(condicionalOimplicacion.test(data))
+        return pcondicionalOimplicacion(data)
+    else if(bicondicionalOequivalencia.test(data))
+        return pbicondicionalOequivalencia(data)
 }
 
 const doubleDenial=(data)=>{
@@ -30,7 +43,37 @@ const oneDenial=(data)=>{
     return [[characterN,['v','f']],[data,['f','v']]]
 }
 const pConjucion=(data)=>{
-    const firstF=/^[\s]*[a-z][[\s]*/
-    const firstD=firstF.exec(data)[0]
-    console.log(firstD);
+    const firstF=/^[\s]*[(]?[\s]*[a-z]/;
+    const firstD=firstF.exec(data)[0].replace(/\s+/g, ' ').length==2?firstF.exec(data)[0][1]:firstF.exec(data)[0];
+    const lastF=/[∧][\s]*[a-z]/
+    const lastD=lastF.exec(data)[0].length==2?lastF.exec(data)[0][1]:lastF.exec(data)[0][2]
+    return [[firstD,['v','v','f','f']],[lastD,['v','f','v','f']],[data,['v','f','f','f']]]
+}
+const pdisyuncion=(data)=>{
+    const firstF=/^[\s]*[(]?[\s]*[a-z]/;
+    const firstD=firstF.exec(data)[0].replace(/\s+/g, ' ').length==2?firstF.exec(data)[0][1]:firstF.exec(data)[0];
+    const lastF=/[∨][\s]*[a-z]/
+    const lastD=lastF.exec(data)[0].length==2?lastF.exec(data)[0][1]:lastF.exec(data)[0][2]
+    return [[firstD,['v','v','f','f']],[lastD,['v','f','v','f']],[data,['v','v','v','f']]]
+}
+const pdisyuncionE=(data)=>{
+    const firstF=/^[\s]*[(]?[\s]*[a-z]/;
+    const firstD=firstF.exec(data)[0].replace(/\s+/g, ' ').length==2?firstF.exec(data)[0][1]:firstF.exec(data)[0];
+    const lastF=/[⨁][\s]*[a-z]/
+    const lastD=lastF.exec(data)[0].length==2?lastF.exec(data)[0][1]:lastF.exec(data)[0][2]
+    return [[firstD,['v','v','f','f']],[lastD,['v','f','v','f']],[data,['f','v','v','f']]]
+}
+const pcondicionalOimplicacion=(data)=>{
+    const firstF=/^[\s]*[(]?[\s]*[a-z]/;
+    const firstD=firstF.exec(data)[0].replace(/\s+/g, ' ').length==2?firstF.exec(data)[0][1]:firstF.exec(data)[0];
+    const lastF=/[⟹][\s]*[a-z]/
+    const lastD=lastF.exec(data)[0].length==2?lastF.exec(data)[0][1]:lastF.exec(data)[0][2]
+    return [[firstD,['v','v','f','f']],[lastD,['v','f','v','f']],[data,['v','f','v','v']]] 
+}
+const pbicondicionalOequivalencia=(data)=>{
+    const firstF=/^[\s]*[(]?[\s]*[a-z]/;
+    const firstD=firstF.exec(data)[0].replace(/\s+/g, ' ').length==2?firstF.exec(data)[0][1]:firstF.exec(data)[0];
+    const lastF=/[⟺][\s]*[a-z]/
+    const lastD=lastF.exec(data)[0].length==2?lastF.exec(data)[0][1]:lastF.exec(data)[0][2]
+    return [[firstD,['v','v','f','f']],[lastD,['v','f','f','v']],[data,['v','f','v','v']]] 
 }
